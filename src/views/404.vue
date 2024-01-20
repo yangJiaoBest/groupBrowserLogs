f475ee7506f60d96c5d2eb6884d0b956560bf88b
momobest <yangjiao_best@163.com>
2024-01-20 20:54:23 +0800

    404跳转

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,7 +1,7 @@
 <template>
 	<a-result status="404" title="404" sub-title="Sorry, the page you visited does not exist.">
 		<template #extra>
-			<router-link class="ant-btn ant-btn-primary" to="/">Back Home</router-link>
+			<router-link class="ant-btn ant-btn-primary" to="/login">去登录</router-link>
 		</template>
 	</a-result>
 </template>
